8cf830b43880927e571150598f50591ba9033ce6
KJRana2003 <111540332+KJRana2003@users.noreply.github.com>
2022-08-18 18:39:36 -0400

    Account update

--- ver2/Account.java
+++ ver2/Account.java
@@ -1,15 +1,10 @@
 package ver2;
 
 public class Account {
-	//New Comment for balance
 	// Instance variable, only available inside this class.
-	private double balance;
+	private double balance;//New Comment for balance
 	private String name;
-	// Constructor
-	public Account(double balance) {
-		this("Unknown", balance);
-	}
-
+	
 	
 	public Account(String name, double balance) {
 		this.name = name;
